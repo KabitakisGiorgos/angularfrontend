6580c9fd12743ea8d93477c1b0ea1b60b2adcbd6
GKampitakis <csd3142@gmail.com>
2018-09-21 01:33:36 +0300

    Need to fix the uploading of an image and make it appear

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import {
   Component,
   OnInit
 } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 declare function test(callback): any;
 
@@ -12,7 +13,8 @@ declare function test(callback): any;
 })
 export class AppComponent implements OnInit {
   title = 'testAngularFirebase';
-  constructor() {}
+  uploaded:boolean = false;
+  constructor(private http:HttpClient) {}
 
   ngOnInit() {
     test((test) => {
@@ -20,4 +22,44 @@ export class AppComponent implements OnInit {
       console.log(test);
     });
   }
+
+  appear(){
+
+  }
+
+  // constructor(private serversService: ServersService,
+  //   private router: Router,
+  //   private route: ActivatedRoute,private http:HttpClient) {}
+
+  // ngOnInit() {
+  //   this.servers = this.serversService.getServers();
+  // }
+  // selectedFile:File = null;
+  // uploaded:boolean=false;
+ 
+  // test(){
+  //   this.uploaded=!this.uploaded;
+  //   console.log(this.uploaded);
+  // }
+
+  // onReload() {
+  //   // this.router.navigate(['servers'], {relativeTo: this.route});
+  // }
+
+  // onFileSelected(event) {
+  //   // console.log(event);
+  //   this.selectedFile =<File> event.target.files[0];
+  // }
+
+  // onUpload() {
+  //   const fd=new FormData();
+  //   fd.append('file-to-upload',this.selectedFile,this.selectedFile.name);
+  //   this.http.post('http://localhost:4201/test',fd, {responseType: 'text'}).subscribe(
+  //     (res)=>{
+  //       console.log(res);
+  //       this.uploaded=true;
+  //     }
+  //   )
+  // }
+
 }
