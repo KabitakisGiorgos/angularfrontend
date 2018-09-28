7d12522e42ce76e76110434a9c5d407654573c6f
Giorgos Kampitakis <csd3142@gmail.com>
2018-09-28 18:35:02 +0300

    a fcking working state with animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+
 var config = require('./config.json');
 import {
   Component,
@@ -8,62 +9,76 @@ import {
 import {
   HttpClient
 } from '@angular/common/http';
+import { fade } from './animation';
 
 declare function test(callback): any;
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: fade
 })
 export class AppComponent implements OnInit {
+
+  state = 'in';
+  enableAnimation = false;
   title = 'testAngularFirebase';
   @ViewChild('fileInput') fileInput: ElementRef;
   @ViewChild('fileInput2') fileInput2: ElementRef;
   selectedPicture: String;
   index: number = 0;
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
     test((test) => {
-      if(test.body==='next') this.fileInput2.nativeElement.click();
-      else if(test.body==='prev') this.fileInput.nativeElement.click();
+      if (test.body === 'next') this.fileInput2.nativeElement.click();
+      else if (test.body === 'prev') this.fileInput.nativeElement.click();
       console.log(test);
     });
     this.selectedPicture = config.pictures[this.index].path;
   }
 
   nextPicture() {
-    try {
-      this.selectedPicture = config.pictures[++this.index].path;
-    } catch (err) {
-      this.index = 0;
-      this.selectedPicture = config.pictures[this.index].path;
-    }
-    console.log('next');
+    this.enableAnimation = true;
+    this.toggleState();
+    setTimeout(()=>{
+      try {
+        this.selectedPicture = config.pictures[++this.index].path;
+      } catch (err) {
+        this.index = 0;
+        this.selectedPicture = config.pictures[this.index].path;
+      }
+      this.toggleState();
+    },1000);
   }
 
   prevPicture() {
-    try {
-      this.selectedPicture = config.pictures[--this.index].path;
-    } catch (err) {
-      this.index = config.pictures.length-1;
-      this.selectedPicture = config.pictures[this.index].path;
-    }
-    console.log('prev');
+    this.enableAnimation = true;
+    this.toggleState();
+    setTimeout(()=>{
+      try {
+        this.selectedPicture = config.pictures[--this.index].path;
+      } catch (err) {
+        this.index = config.pictures.length - 1;
+        this.selectedPicture = config.pictures[this.index].path;
+      }
+      this.toggleState();
+      console.log('prev');
+    },1000) 
   }
 
+  onClick($event) {
+    
+    setTimeout(() => {
+      // if($event.currentTarget.innerHTML==='Next') this.nextPicture();
+      // else if($event.currentTarget.innerHTML==='Previous')
+      this.prevPicture();
+      console.log($event);
+    }, 1000)
 
+  }
 
-  // onUpload() {
-  //   const fd=new FormData();
-  //   fd.append('file-to-upload',this.selectedfile,this.selectedfile.name);
-  //   this.http.post('http://localhost:4201/test',fd, {responseType: 'text'}).subscribe(
-  //     (res)=>{
-  //       console.log(res);
-  //       this.uploaded=true;
-  //     }
-  //   )
-  // }
-
+  toggleState() {
+    this.state = this.state === 'in' ? 'out' : 'in';
+  }
 }
